3a0a96acb4ff84e9b34986b5b6c1264ba60895a2
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-21 20:01:25 +0200

    Adding some logic for the printer

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,8 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Printer printer = new Printer("Duplex", 60, 20);
+        printer.fillUp(2);
+        printer.printPage();
     }
 }
--- src/com/timbuchalka/Printer.java
+++ src/com/timbuchalka/Printer.java
@@ -1,16 +1,64 @@
 package com.timbuchalka;
 
 public class Printer {
-    private String type;
+    private boolean duplex;
     private int tonerLevel; //percentage
-    private int pagesLeft;
     private int pagesPrinted;
+    private int pagesLeft;
 
-    public void fillUp() {
-        System.out.println("Filling up the toner");
+    public Printer(boolean duplex, int tonerLevel, int pagesPrinted, int pagesLeft) {
+        this.duplex = duplex;
+        this.tonerLevel = tonerLevel;
+        this.pagesPrinted = pagesPrinted;
+        this.pagesLeft = pagesLeft;
     }
 
-    public void printPage() {
-        System.out.println("Printing a page");
+    public int getPagesLeft() {
+        return pagesLeft;
+    }
+
+    public void setPagesLeft(int pagesLeft) {
+        this.pagesLeft = pagesLeft;
+    }
+
+    public boolean isDuplex() {
+        return duplex;
+    }
+
+    public void setDuplex(boolean duplex) {
+        this.duplex = duplex;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public void setTonerLevel(int tonerLevel) {
+        this.tonerLevel = tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public void setPagesPrinted(int pagesPrinted) {
+        this.pagesPrinted = pagesPrinted;
+    }
+
+    public void fillUp(int ink) {
+        System.out.println("Filling up the toner");
+        this.tonerLevel += ink;
+    }
+
+    public void printPage(int quantity, boolean duplex) {
+        if (duplex) {
+            this.tonerLevel = this.tonerLevel - quantity * 2;
+            this.pagesLeft -= quantity * 2;
+            System.out.println("Printing a page");
+        } else {
+            this.tonerLevel = this.tonerLevel - quantity;
+            this.pagesLeft -= quantity;
+            System.out.println("Printing a page");
+        }
     }
 }
