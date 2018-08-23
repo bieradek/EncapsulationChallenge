b5ab1bf5d57ab06df7e37be0856d01fcef916089
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-23 19:14:12 +0200

    Adding some code

--- src/com/TimsCode/Printer.java
+++ src/com/TimsCode/Printer.java
@@ -1,4 +1,28 @@
 package com.TimsCode;
 
 public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel >= 1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0; // during the initialization set the pages printed to zero
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            int tonerAdded = 0;
+            return something;
+
+        } else {
+            return -1;
+        }
+    }
+
 }
