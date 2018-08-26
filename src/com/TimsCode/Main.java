1f5b819e957beccd23a6f634bb2cbc925af1ede9
Radosław Biedrzycki <biedrzycki.radek@gmail.com>
2018-08-26 23:27:32 +0200

    Uploading final code

--- src/com/TimsCode/Main.java
+++ src/com/TimsCode/Main.java
@@ -3,9 +3,11 @@ package com.TimsCode;
 public class Main {
 
     public static void main(String[] args) {
-        Printer somePrinter = new Printer(50, true);
+        Printer somePrinter = new Printer(50, false); // true
         System.out.println("Initial page count = " + somePrinter.getPagesPrinted());
         int pagesPrinted = somePrinter.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + somePrinter.getPagesPrinted());
+        pagesPrinted = somePrinter.printPages(2);
         System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + somePrinter.getPagesPrinted());
     }
 }
