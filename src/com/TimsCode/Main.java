d0056a4bcb803d7820dae14b31a8b6dc50fe402f
Radosław Biedrzycki <biedrzycki.radek@gmail.com>
2018-08-26 23:02:40 +0200

    Uploading some code

--- src/com/TimsCode/Main.java
+++ src/com/TimsCode/Main.java
@@ -3,5 +3,9 @@ package com.TimsCode;
 public class Main {
 
     public static void main(String[] args) {
+        Printer somePrinter = new Printer(50, true);
+        System.out.println("Initial page count = " + somePrinter.getPagesPrinted());
+        int pagesPrinted = somePrinter.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + somePrinter.getPagesPrinted());
     }
 }
--- src/com/TimsCode/Printer.java
+++ src/com/TimsCode/Printer.java
@@ -17,12 +17,27 @@ public class Printer {
 
     public int addToner(int tonerAmount) {
         if (tonerAmount > 0 && tonerAmount <= 100) {
-            int tonerAdded = 0;
-            return something;
-
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            }
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
         } else {
             return -1;
         }
     }
 
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
 }
